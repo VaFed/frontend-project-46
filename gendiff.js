1a3e44b26cbfcb0d8ec34842b52ab71bad4b66f1
DFedyakov <elvis4272@gmail.com>
2023-05-20 23:22:13 +0500

    options

---
+++ gendiff.js
@@ -0,0 +1,12 @@
+import { program } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .usage('[options]')
+  .description('Compares two configuration files and shows a difference.')
+  .version('-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse(process.argv);
