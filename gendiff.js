e685af7ce3b76e49c207b44011330fea3174800c
DFedyakov <elvis4272@gmail.com>
2023-05-24 17:55:39 +0500

    some problem

--- gendiff.js
+++ gendiff.js
@@ -4,9 +4,16 @@ const program = new Command();
 
 program
   .name('gendiff')
-  .usage('[options]')
+  .usage('[options] <filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>',  'output format')
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
+
+  // console.log(program);
 
 program.parse(process.argv);
